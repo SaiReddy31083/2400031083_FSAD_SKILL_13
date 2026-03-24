7d5e5de4f738a5083a1dd0b958df232687902be5
SaiReddy31083 <2400031083@kluniversity.in>
2026-03-24 12:24:07 +0530

    FSAD Skill 13 full stack deployment project

---
+++ src/main/java/klu/skill13/Sklill13Application.java
@@ -0,0 +1,13 @@
+package klu.skill13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sklill13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sklill13Application.class, args);
+	}
+
+}
---
+++ src/test/java/klu/skill13/Sklill13ApplicationTests.java
@@ -0,0 +1,13 @@
+package klu.skill13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sklill13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
